77cd90ce40bdbb73fe65c16baeb8eaeb9c566eeb
Igor Kim <igor.skh@gmail.com>
2017-04-12 21:38:11 +0200

    First commit

---
+++ FSVRReader.py
@@ -0,0 +1,92 @@
+"""
+Author: Igor Kim
+E-mail: igor.skh@gmail.com
+
+Reader module for R&S FSVR Signal Analyzer
+"""
+
+import os.path
+import matplotlib.pyplot as plt
+import time
+import datetime
+
+
+class FSVRReader:
+    file_path = ""
+    next_line = 1
+    next_frame = 0
+    last_frame = {}
+    file = None
+    header = {}
+
+    threshold = 0
+    filter = []
+
+    '''
+    Read one line of a file, increases a next_line counter
+    '''
+    def read_line(self):
+        result = self.file.readline()
+        self.next_line += 1
+        return result
+
+    '''
+    Init function, inits file
+    '''
+    def __init__(self, filename):
+        if os.path.isfile(filename):
+            self.next_line = 1
+            self.file_path = filename
+            self.file = open(self.file_path, "r")
+
+    '''
+    Reads file header, must be used first after initialization
+    '''
+    def read_header(self, limit = 25):
+        for i in range(limit):
+            values = self.read_line().rstrip().split(";")
+            self.header[values[0]] = values[1:]
+            if values[0] == "Frame":
+                return True
+        return False
+
+    '''
+    Reads next data frame
+    '''
+    def read_frame(self):
+        frame = {}
+        frame_data = {}
+        # read the number provided by Values number in the header
+        for i in range(int(self.header['Values'][0])+2):
+            values = self.read_line().rstrip().split(";")
+            # for the frame header data
+            if values[0] in ['Frame', 'Timestamp']:
+                # evaluate timestamp
+                if values[0] == 'Timestamp':
+                    frame['Date'] = values[1]
+                    frame['Time'] = values[2]
+                    #12.Apr 17;17:55:58.470
+                    # TODO: milliseconds do not saved in a timestamp format
+                    frame['Timestamp'] = time.mktime(datetime.datetime.strptime(frame['Date']+"T"+frame['Time'], "%d.%b %yT%H:%M:%S.%f").timetuple())
+                    # temporary fix
+                    frame['Timestamp'] += float(frame['Time'][-4:])
+                else:
+                    frame[values[0]] = values[1]
+            # for values
+            else:
+                frame_data[float(values[0])] = float(values[1])
+        self.last_frame = frame
+        frame['Data'] = frame_data
+
+        return frame
+
+    def plot_frame(self, frame={}):
+        if len(frame) == 0:
+            frame = self.last_frame
+        plt.plot(list(frame['Data'].keys()), list(frame['Data'].values()))
+        plt.xlabel(self.header['x-Unit'][0])
+        plt.ylabel(self.header['y-Unit'][0])
+        #value = datetime.datetime.fromtimestamp(frame['Timestamp'])
+        #print(frame['Timestamp'], value.strftime("%d.%b %yT%H:%M:%S.%f"), frame['Date']+"T"+frame['Time'])
+        plt.title(frame['Date']+" "+ frame['Time'] + " " +str(frame['Timestamp']) +" " +  " Frame #"+ frame['Frame'])
+        plt.savefig("test.png")
---
+++ main.py
@@ -0,0 +1,45 @@
+"""
+Author: Igor Kim
+E-mail: igor.skh@gmail.com
+
+Reader module for R&S FSVR Signal Analyzer DAT files
+"""
+from FSVRReader import FSVRReader
+import matplotlib.pyplot as plt
+
+
+def time_delta_eval(reader, n=10):
+    """
+    Evaluates delta time between frames
+    """
+    time_delta = []
+    last_time = 0
+    for i in range(n):
+        reader.read_frame()
+        if i == 0:
+            time_delta.append(0)
+        else:
+            time_delta.append(last_time - reader.last_frame['Timestamp'])
+        last_time = reader.last_frame['Timestamp']
+
+    return time_delta
+
+
+if __name__ == '__main__':
+    reader = FSVRReader("252MKS_001.DAT")
+    reader.threshold = -75
+
+    # read DAT file header
+    reader.read_header()
+    # print(reader.header, reader.next_line)
+    # 35
+    # 39 empty
+    # evaluate time difference
+    td = time_delta_eval(reader,35)
+    #
+    # plt.plot(td, "ro")
+    # plt.xlabel("Frame")
+    # plt.ylabel("Time Difference")
+    # plt.savefig("time_delta_eval.png")
+
+    reader.plot_frame()
