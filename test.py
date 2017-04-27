2e3cbe89186df849b0c96b90e244a9b36a3e2f7c
Igor Kim <igor.skh@gmail.com>
2017-04-27 21:29:11 +0200

    add comments

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 """
 Author: Igor Kim
 E-mail: igor.skh@gmail.com
+Repository: https://bitbucket.org/igorkim/fsvrreader
 
 Reader module for R&S FSVR Signal Analyzer DAT files
 
@@ -9,10 +10,16 @@ April 2017
 from FSVRAnalysis import FSVRAnalysis
 from FSVRReader import FSVRReader
 
+# initialize reader object, could be different for different devices
 reader = FSVRReader("dat/500mks.DAT")
+# initialize analyzer main object
 analyzer = FSVRAnalysis(reader)
+# set number of points to analyze, or 0 if all points
 analyzer.set_data_points(500)
+# set reference level
 analyzer.set_threshold(-80)
+# get main information from the data file
 analyzer.get_info()
+# do things...
 analyzer.last_frame_plot()
 analyzer.avg_values_plot()
